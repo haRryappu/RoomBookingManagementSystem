256eb7dc7cf48cd15bbc8ce9a92f65dcee601a7c
haRryappu <harilr2210@gmail.com>
2020-06-17 12:34:49 +0530

    Code Challenge

---
+++ RoomBookingManagementSystem/src/test/java/com/rbms/RoomBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
